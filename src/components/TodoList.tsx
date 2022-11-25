3e5d1bb1203562f5f653012e786c6940d9a60e06
Benjamin Desole <benjamin.desole@gmail.com>
2022-11-25 22:57:38 +0100

    Update TodoList.tsx
    display message if no todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import { List, Paper, Container } from "@mui/material";
+import { List, Paper, Container, Typography } from "@mui/material";
 import React from "react";
 import { TodoContext } from "../context/TodoContext";
 import { TodoContextType, ITodo } from "../types/todo";
@@ -13,14 +13,20 @@ const TodoList = () => {
     <Container maxWidth="sm">
       <Paper elevation={3}>
         <List>
-          {todos.map((todo: ITodo, index: number) => (
-            <Todo
-              key={todo.id}
-              todo={todo}
-              handleTodoStateChange={() => handleTodoStateChange(index)}
-              onDeleteTodoClick={() => onDeleteTodoClick(index)}
-            />
-          ))}
+          {todos.length > 0 ? (
+            todos.map((todo: ITodo, index: number) => (
+              <Todo
+                key={todo.id}
+                todo={todo}
+                handleTodoStateChange={() => handleTodoStateChange(index)}
+                onDeleteTodoClick={() => onDeleteTodoClick(index)}
+              />
+            ))
+          ) : (
+            <Typography fontWeight={"bold"} align="center">
+              No task created ...
+            </Typography>
+          )}
         </List>
       </Paper>
     </Container>
