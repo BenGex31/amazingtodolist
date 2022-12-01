bb43cf4f10fadc89141f1267b98adf1eabf71769
Benjamin Desole <benjamin.desole@gmail.com>
2022-12-01 23:43:03 +0100

    test AppTitle component

---
+++ src/AppTitle.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import AppTitle from "./components/AppTitle";
+
+test("renders app title", () => {
+  render(<AppTitle />);
+  const title = screen.getByTestId("AppTitle");
+  expect(title).toBeInTheDocument();
+  expect(title.textContent).toEqual("Amazing Todo List");
+});
--- src/components/AppTitle.tsx
+++ src/components/AppTitle.tsx
@@ -4,7 +4,12 @@ import React from "react";
 const AppTitle = () => {
   return (
     <Paper sx={{ backgroundColor: "#617BFF" }} elevation={3}>
-      <Typography color={"#10069F"} align="center" variant="h1">
+      <Typography
+        data-testid="AppTitle"
+        color={"#10069F"}
+        align="center"
+        variant="h1"
+      >
         Amazing Todo List
       </Typography>
     </Paper>
